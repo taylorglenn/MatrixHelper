f6a2c922bcf67a4f84d99221ccb2c07ca9ab7138
Taylor <taylorglenn@live.com>
2016-10-13 13:07:00 -0500

    Fixed the multiplication issue.

---
+++ MatrixHelperGUI/GetDoubleGUI.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MatrixHelperGUI
+{
+    partial class GetDoubleGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(9, 84);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(145, 38);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(9, 52);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(145, 26);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(8, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(201, 20);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Enter a double or a fraction";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(8, 29);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(354, 20);
+            this.lblError.TabIndex = 3;
+            this.lblError.Text = "What was entered was not a double or a fraction.";
+            this.lblError.Visible = false;
+            // 
+            // GetDoubleGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 132);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnOK);
+            this.Name = "GetDoubleGUI";
+            this.Text = "Enter Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblError;
+    }
+}
